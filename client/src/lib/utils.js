c1a4f52365356f13ae6837000dcd10cd0969800b
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-06-07 17:43:16 +0300

    add chat container

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,6 +1,8 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import animationData from "@/assets/lottie-json";
+
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
@@ -18,4 +20,10 @@ export const getColor = (color) => {
   }
 
   return colors[0]; // Fallback to the first color if out of range
+};
+
+export const animationDefaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData,
 };
