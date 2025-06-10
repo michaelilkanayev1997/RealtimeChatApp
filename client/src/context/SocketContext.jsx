34bf08f445eb67c6dd60a1a250f2961e337c0ee7
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-06-10 22:50:52 +0300

    add Send/Receive Message main logic

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -24,6 +24,21 @@ export const SocketProvider = ({ children }) => {
         console.log("Connected to socket server.");
       });
 
+      const handleRecieveMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore();
+
+        if (
+          selectedChatType !== undefined &&
+          (selectedChatData._id === message.sender._id ||
+            selectedChatData._id === message.recipient._id)
+        ) {
+          addMessage(message);
+        }
+      };
+
+      socket.current.on("recieveMessage", handleRecieveMessage);
+
       return () => {
         socket.current.disconnect();
       };
--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,5 @@
 import { Server as SocketIOServer } from "socket.io";
+import Message from "./models/MessagesModel.js";
 
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
@@ -22,6 +23,24 @@ const setupSocket = (server) => {
     }
   };
 
+  const sendMessage = async (message) => {
+    const senderSocketId = userSocketMap.get(message.senderId);
+    const recipientSocketId = userSocketMap.get(message.recipientId);
+
+    const createdMessage = await Message.create(message);
+
+    const messageData = await Message.findById(createdMessage._id)
+      .populate("sender", "id email firstName lastName image color")
+      .populate("recipient", "id email firstName lastName image color");
+
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit("recieveMessage", messageData);
+    }
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("recieveMessage", messageData);
+    }
+  };
+
   io.on("connection", (socket) => {
     console.log("New user connected:", socket.id);
 
@@ -34,6 +53,7 @@ const setupSocket = (server) => {
       console.log("User ID not provided during connection.");
     }
 
+    socket.on("sendMessage", sendMessage);
     socket.on("disconnect", () => disconnect(socket));
   });
 };
