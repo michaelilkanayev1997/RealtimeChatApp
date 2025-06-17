85026587600fa848fc89a19cb8c20e9bdf98f789
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-06-17 20:01:38 +0300

    add endpoint to retrieve all contacts

--- server/controllers/ContactsController.js
+++ server/controllers/ContactsController.js
@@ -97,3 +97,21 @@ export const getContactsForDMList = async (req, res, next) => {
     res.status(500).send("Internal server error");
   }
 };
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const users = await User.find(
+      { _id: { $ne: req.userId } },
+      "firstName lastName _id email"
+    );
+
+    const contacts = users.map((user) => ({
+      label: user.firstName ? `${user.firstName} ${user.lastName}` : user.email,
+    }));
+
+    return res.status(200).json({ contacts });
+  } catch (error) {
+    console.error("Error searching contacts:", error);
+    res.status(500).send("Internal server error");
+  }
+};
